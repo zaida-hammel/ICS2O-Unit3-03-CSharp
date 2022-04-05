1dc0a064a4bdd8c99ffa379c185ecf69a9e6fd36
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-05 20:21:44 +0000

    edited code :]

--- main.cs
+++ main.cs
@@ -2,3 +2,28 @@
 // Created on: April 2022
 //
 // This program accepts user input;
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double radius;
+        double volume;
+        //input
+        Console.WriteLine("This program finds the volume of a sphere.");
+        Console.WriteLine("");
+        Console.WriteLine("Formula = 4/3 πr³");
+        Console.WriteLine("");
+        Console.Write("Enter the radius of the pyramid: ");
+        radius = double.Parse(Console.ReadLine());
+        Console.WriteLine("");
+        //process
+        volume = (double)4 / 3 * Math.PI * Math.Pow(radius, 3);
+        Console.Write("Volume of the sphere = " + volume.ToString("0.00"));
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
+}
